7c793732315e870d06db81a32fd13eca81002c8a
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-12-28 18:14:11 +0000

    add more comments

--- src/Menu.h
+++ src/Menu.h
@@ -6,15 +6,23 @@
 #include "database/read.h"
 #include <limits>
 
+// Function to display menu
 void Menu(std::string folder);
 void quantity();
 void listing();
-void bestFlights();
 void menuFlights();
 void menuDestination();
 
+// Function to handle the process of finding the best flight
+void bestFlights();
+
+// Function to filter airplanes
 vector<string> filterAirplanes();
+
+// Function to select type of origin or destination
 int selectType(std::string arg);
+
+// Functions to input information based on type and flag
 std::string typeAirport(std::string type, int flag);
 pair<std::string, std::string> typeCity(std::string type, int flag);
 pair<double, double> typeCoordinates(std::string type, int flag);
