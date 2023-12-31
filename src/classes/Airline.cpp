35ccdafa0b58cfc2eb770bfaa8a9e059dbc888c3
duardoliveiras <duardooliveiras@gmail.com>
2023-12-31 19:32:12 +0000

    add airline doxygen

--- src/classes/Airline.cpp
+++ src/classes/Airline.cpp
@@ -1,50 +1,109 @@
 #include "Airline.h"
 
-// Constructor for Airline class
+/**
+ * @brief Constructor for Airline class
+ * Complexity: O(1)
+ * @param code The code of the airline.
+ * @param name The name of the airline.
+ * @param callsign The callsign of the airline.
+ * @param country The country of the airline.
+ */
 Airline::Airline(std::string code, std::string name, std::string callsign,
-                 std::string country) {
+                 std::string country)
+{
   this->code = code;
   this->name = name;
   this->callsign = callsign;
   this->country = country;
 }
 
-// Constructor for Airline code class
+/**
+ * @brief Constructor for Airline class
+ * Complexity: O(1)
+ * @param code The code of the airline.
+ */
 Airline::Airline(std::string code) { this->code = code; }
 
-// Function for comparing two  Airline code
-bool Airline::operator==(const Airline &other) const {
+/**
+ * @brief Function for comparing two Airline code
+ * Complexity: O(1)
+ * @param other The other airline.
+ * @return bool True if the two airlines have the same code.
+ */
+bool Airline::operator==(const Airline &other) const
+{
   return this->code == other.code;
 }
 
-// Function to print the Aiport information
-void Airline::display() {
+/**
+ * @brief Function to print the Airline information
+ * Complexity: O(1)
+ * @return void
+ */
+void Airline::display()
+{
   std::cout << "Code: " << this->code << " / ";
   std::cout << "Name: " << this->name << " / ";
   std::cout << "Callsign: " << this->callsign << " / ";
   std::cout << "Country: " << this->country << " / ";
 }
 
-// Setter for set/modifying the Airline code
+/**
+ * @brief Setter for set/modifying the Airline code
+ * Complexity: O(1)
+ * @param code The code of the airline.
+ * @return void
+ */
 void Airline::setCode(std::string code) { this->code = code; }
 
-// Setter for set/modifying the Airline name
+/**
+ * @brief Setter for set/modifying the Airline name
+ * Complexity: O(1)
+ * @param name The name of the airline.
+ * @return void
+ */
 void Airline::setName(std::string name) { this->name = name; }
 
-// Setter for set/modifying the Airline callsign
+/**
+ * @brief Setter for set/modifying the Airline callsign
+ * Complexity: O(1)
+ * @param callsign The callsign of the airline.
+ * @return void
+ */
 void Airline::setCallsign(std::string callsign) { this->callsign = callsign; }
 
-// Setter for set/modifying the Airline country
+/**
+ * @brief Setter for set/modifying the Airline country
+ * Complexity: O(1)
+ * @param country The country of the airline.
+ * @return void
+ */
 void Airline::setCountry(std::string country) { this->country = country; }
 
-// Getter for retrieving Airline code
+/**
+ * @brief Getter for retrieving Airline code
+ * Complexity: O(1)
+ * @return std::string The code of the airline.
+ */
 std::string Airline::getCode() { return code; }
 
-// Getter for retrieving Airline name
+/**
+ * @brief Getter for retrieving Airline name
+ * Complexity: O(1)
+ * @return std::string The name of the airline.
+ */
 std::string Airline::getName() { return name; }
 
-// Getter for retrieving Airline callsign
+/**
+ * @brief Getter for retrieving Airline callsign
+ * Complexity: O(1)
+ * @return std::string The callsign of the airline.
+ */
 std::string Airline::getCallsign() { return callsign; }
 
-// Getter for retrieving Airline country
+/**
+ * @brief Getter for retrieving Airline country
+ * Complexity: O(1)
+ * @return std::string The country of the airline.
+ */
 std::string Airline::getCountry() { return country; }
