04e55ae4b358eddb10703bf9456f3ec0e2903314
duardoliveiras <duardooliveiras@gmail.com>
2023-12-31 19:29:35 +0000

    add main doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "Menu.h"
 
+/**
+ * @brief Main function to run the program and load the database of airports and flights
+ * Complexity: O(1)
+ * @param argc The number of arguments. If no argument is passed, the default folder is "fake".
+ * @param argv The arguments (folder name).
+ * @return int 0 if the program runs successfully.
+ */
 int main(int argc, char const *argv[])
 {
   if (argc < 2)
